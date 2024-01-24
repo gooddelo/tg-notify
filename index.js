3ab376643ff3738da56503168dc1a745235166ab
Pavel <tojefin@gmail.com>
2024-01-24 19:37:46 +0400

    urlencode

--- index.js
+++ index.js
@@ -30,14 +30,7 @@ app.post('/api/v1/sendform/', async (req, res) => {
 		return res.status(401).end()
 	}
 
-	let text = `
-	Получена заявка \n
-	${req.get('Referrer')} \n
-	👨: ${Name} \n
-	📞: ${Phone} \n
-	📧: ${Email} \n
-	📄: ${Comment} 
-	`
+	let text = urlencode(`Получена заявка \n${req.get('Referrer')} \n👨: ${Name} \n📞: ${Phone} \n📧: ${Email} \n📄: ${Comment}`)
 
 	await axios.get(`https://api.telegram.org/bot${BOTTOKEN}/sendMessage?text=${text}&chat_id=${CHAT}`)
 
